049993c63ef79a560b56f2c9724e76b639a81e62
PTL <PTL@Pragmatic>
2015-02-17 15:45:09 +0530

    add comments

--- src/com/ptl/testcases/InmateRegistrationTest.java
+++ src/com/ptl/testcases/InmateRegistrationTest.java
@@ -37,6 +37,7 @@ public class InmateRegistrationTest extends TestBase {
 		System.out.println("************************************************");
 
 		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		//add comment
 		
 		
 		if (!isLoggedIn) {
