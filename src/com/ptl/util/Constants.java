579dc29e80604dd5b5e7b6ce339ee0175b95376e
Sahan <sahan.ratnayake@pragmatictesters.com>
2015-02-18 16:00:32 +0530

    Constant file update

--- src/com/ptl/util/Constants.java
+++ src/com/ptl/util/Constants.java
@@ -54,6 +54,9 @@ public class Constants {
 		public static final String PasswordErrorText = "Invalid login or password";
 		public static final String Loginline2 = "//*[@id='login-form-login']";
 		public static final String emptylogin = "";
+		public static final String passwordline2 = "//*[@id='login-form-password']";
+		public static final String emptyPassword = "";
+
 
 		// Home Page
 		public static final String Home_PageHeader = "html/body/div[2]/div[2]/div/div/h1";
