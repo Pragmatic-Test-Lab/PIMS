66cad58eb6770310667767ab7df8ed1940cc40f9
PTL <PTL@Pragmatic>
2015-02-18 14:20:34 +0530

    new addmission

--- src/com/ptl/pages/NewAdmissionPage.java
+++ src/com/ptl/pages/NewAdmissionPage.java
@@ -1,15 +1,95 @@
 package com.ptl.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.ptl.util.Constants;
 
 public class NewAdmissionPage {
 	
 	WebDriver driver;
 	
+	@FindBy(xpath = Constants.CreateAdmission_InmateCategory)
+	WebElement InmateCatagory;
+	@FindBy(xpath = Constants.CreateAdmission_CourtWarant)
+	WebElement CourtWarant;
+	@FindBy(xpath = Constants.CreateAdmission_AgeAdmission)
+	WebElement AgeAddmission;
+	@FindBy(xpath = Constants.CreateAdmission_AgeCategory)
+	WebElement AgeCatagory;
+	@FindBy(xpath = Constants.CreateAdmission_RegistrationNumber)
+	WebElement RegNumber;
+	@FindBy(xpath = Constants.CreateAdmission_DateAdmission)
+	WebElement DateAddmission;
+	@FindBy(xpath = Constants.CreateAdmission_MealType)
+	WebElement MealType;
+	@FindBy(xpath = Constants.CreateAdmission_Biometric)
+	WebElement BioMetric;
+	@FindBy(xpath = Constants.CreateAdmission_NameWarrant)
+	WebElement NameWarrent;
+	@FindBy(xpath = Constants.CreateAdmission_OccurenceClassification)
+	WebElement OccurenceClassificatio;
+	@FindBy(xpath = Constants.CreateAdmission_Gender)
+	WebElement Gender;
+	@FindBy(xpath = Constants.CreateAdmission_CurrentPrisonInstitute)
+	WebElement CurrentPositionInstitute;
+	@FindBy(xpath = Constants.CreateAdmission_ImageRHSBrowse)
+	WebElement ImageRHSBrowse;
+	@FindBy(xpath = Constants.CreateAdmission_ImageRHSRemove)
+	WebElement ImageRHSRemove;
+	@FindBy(xpath = Constants.CreateAdmission_ImageFrontRemove)
+	WebElement ImageFrontBrowse;
+	@FindBy(xpath = Constants.CreateAdmission_ImageFrontRemove)
+	WebElement ImageFrontRemove;
+	@FindBy(xpath = Constants.CreateAdmission_ImageLHSBrowse)
+	WebElement ImageLHSBrowse;
+	@FindBy(xpath = Constants.CreateAdmission_ImageLHSRemove)
+	WebElement ImageLHSRemove;
+	
+	@FindBy(xpath = Constants.CreateAdmission_InmateCaseDetailTab)
+	WebElement InmateCaseDetailsTab;
+	@FindBy(xpath = Constants.CreateAdmission_InmateCaseDetail_AddNew)
+	WebElement InmateAddNewCase;
+	@FindBy(xpath = Constants.CreateAdmission_InmateCaseDetail_Court)
+	WebElement InmateCaseDetailsCourt;
+	@FindBy(xpath = Constants.CreateAdmission_InmateCaseDetail_CaseNumber)
+	WebElement InmateCaseDetailsCaseNumber;
+	@FindBy(xpath = Constants.CreateAdmission_InmateCaseDetail_DateConviction)
+	WebElement InmateCaseDetailsDateConviction;
+	@FindBy(xpath = Constants.CreateAdmission_InmateCaseDetail_Action)
+	WebElement InmateCaseDetailsAction;
+	
+	@FindBy(xpath = Constants.CreateAdmission_HealthConditionTab)
+	WebElement InmateHealthConditionTab;
+	@FindBy(xpath = Constants.CreateAdmission_HealthCondition_AddNew)
+	WebElement InmateHealthConditionAddNew;
+	@FindBy(xpath = Constants.CreateAdmission_HealthCondition_MedicalAttention)
+	WebElement InmateHealthConditionMedicalAttention;
+	@FindBy(xpath = Constants.CreateAdmission_HealthCondition_Pregnant)
+	WebElement InmateHealthConditionPregnant;
+	@FindBy(xpath = Constants.CreateAdmission_HealthCondition_Remarks)
+	WebElement InmateHealthConditionRemarks;
+	@FindBy(xpath = Constants.CreateAdmission_HealthCondition_ForPreschool)
+	WebElement InmateHealthConditionForPreSchool;
+	@FindBy(xpath = Constants.CreateAdmission_HealthCondition_OfPreschool)
+	WebElement InmateHealthConditionOfPreSchool;
+	@FindBy(xpath = Constants.CreateAdmission_HealthCondition_Action)
+	WebElement InmateHealthConditionAction;
+	
+	@FindBy(xpath = Constants.CreateAdmission_InmateComplainTab)
+	WebElement InmateComplainTab;
+	@FindBy(xpath = Constants.CreateAdmission_PrivatePropertiesTab)
+	WebElement InmatePrivatePropertiesTab;
 	
 	
 	public NewAdmissionPage(WebDriver dr){
 		driver = dr;
+	}
+	
+	public void doCreateNewAdmition(){
+		
+		
 	}
 
 }
