9556df75415a813ec23fb6b6247c143f2b658d49
PTL <PTL@Pragmatic>
2015-02-25 10:09:02 +0530

    Edit and Authorize for Admission

--- src/com/ptl/pims/testcases/NewAdmissionTest.java
+++ src/com/ptl/pims/testcases/NewAdmissionTest.java
@@ -11,7 +11,73 @@ public class NewAdmissionTest extends TestBase{
 
 	HomePage landingPage = null;
 
-	@Test
+	//@Test // PIMS-656
+	public void Test_doCreateNewAdmitionFor_ConvictedInmate(){
+		landingPage = returnToHomePage();		
+		TopMenu topMenu = getTopMenu();
+		NewAdmissionPage newAdmissionPage = topMenu.gotoNewAdmissionPage();
+		newAdmissionPage.doCreateNewAdmitionFor_ConvictedInmate();
+	}
+	
+	//@Test   // PIMS-628
+	public void Test_doCreateNewAdmitionFor_Non_ConvictedInmate() {
+		landingPage = returnToHomePage();		
+		TopMenu topMenu = getTopMenu();
+		NewAdmissionPage newAdmissionPage = topMenu.gotoNewAdmissionPage();
+		newAdmissionPage.doCreateNewAdmitionFor_Un_ConvictedInmate();
+	}
+	
+	//@Test   // PIMS-898
+	public void Test_checkMealTypeIsSelectable() {
+		landingPage = returnToHomePage();		
+		TopMenu topMenu = getTopMenu();
+		NewAdmissionPage newAdmissionPage = topMenu.gotoNewAdmissionPage();
+		newAdmissionPage.checkMealTypeIsSelectable();;
+	}
+	
+	@Test   // PIMS-1190
+	public void Test_checkAgeCatagoryIsAutoSelected() {
+		landingPage = returnToHomePage();		
+		TopMenu topMenu = getTopMenu();
+		NewAdmissionPage newAdmissionPage = topMenu.gotoNewAdmissionPage();
+		newAdmissionPage.checkAgeCatagoryIsAutoSelected();
+	}
+	
+	@Test   // PIMS-19
+	public void Test_checkRegistrationNumberFormtForUnConvictedInamtes() {
+		landingPage = returnToHomePage();		
+		TopMenu topMenu = getTopMenu();
+		NewAdmissionPage newAdmissionPage = topMenu.gotoNewAdmissionPage();
+		newAdmissionPage.checkRegistrationNumberFormtForUnConvictedInamtes();
+	}
+	
+	@Test // PIMS-408
+	public void Test_checkInmateRegistrationNumberIsAutoGenarated(){
+		landingPage = returnToHomePage();		
+		TopMenu topMenu = getTopMenu();
+		NewAdmissionPage newAdmissionPage = topMenu.gotoNewAdmissionPage();
+		newAdmissionPage.checkInmateRegistrationNumberIsAutoGenarated();
+	}
+	
+	@Test // PIMS-869
+	public void Test_checkImateCatagoryIsSelectable(){
+		landingPage = returnToHomePage();		
+		TopMenu topMenu = getTopMenu();
+		NewAdmissionPage newAdmissionPage = topMenu.gotoNewAdmissionPage();
+		newAdmissionPage.checkImateCatagoryIsSelectable();
+	}
+	
+	@Test // PIMS-1198
+	public void Test_checkUserCanUploadTheAllThreeImages() throws Exception{
+		landingPage = returnToHomePage();		
+		TopMenu topMenu = getTopMenu();
+		NewAdmissionPage newAdmissionPage = topMenu.gotoNewAdmissionPage();
+		newAdmissionPage.checkUserCanUploadTheAllThreeImages();
+	}
+	
+	
+	
+	//@Test
 	public void createNewAdmissionTest() throws InterruptedException {
 		landingPage = returnToHomePage();		
 		TopMenu topMenu = getTopMenu();
@@ -22,7 +88,7 @@ public class NewAdmissionTest extends TestBase{
 
 	
 
-	@Test
+	//@Test
 	public void enterInmateCalasificationTest() throws InterruptedException {
 		NewAdmissionPage newAdmissionPage = new NewAdmissionPage(driver);
 		newAdmissionPage.doCreateNewAdmition();
