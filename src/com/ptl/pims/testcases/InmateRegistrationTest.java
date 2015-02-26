371d5787226ae4790875f078c4e7092c23a602bd
Nilenth Selvaraja <nilenth.selvaraja@pragmatictesters.com>
2015-02-26 10:12:16 +0530

    inmate final

--- src/com/ptl/pims/testcases/InmateRegistrationTest.java
+++ src/com/ptl/pims/testcases/InmateRegistrationTest.java
@@ -38,7 +38,7 @@ public class InmateRegistrationTest extends TestBase {
 		//NewAdmissionPage newAdmissionPage = PageFactory.initElements(driver, NewAdmissionPage.class);
 		//String RegNumber = newAdmissionPage.getRegistrationNumber();
 		
-		inmateRegistrationSelect = inmateRegistrationSelect.doSearch("T/20000/2015","" ,"");
+		inmateRegistrationSelect = inmateRegistrationSelect.doSearch("T/10005/2015","" ,"");
 
 		inmateRegistration = inmateRegistrationSelect.clickFirstInmate(); 
 
